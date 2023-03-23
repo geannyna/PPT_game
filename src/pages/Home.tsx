544d25e07bad741e3107b32ed3a5018df53779cb
Claudia <claudia.iacob@hotmail.com>
2023-03-23 19:41:43 +0100

    Todos los elementos listos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -7,16 +6,40 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>
+            <h1>Rock Paper Scissors</h1>
+            </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">RED page</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <p className='rules' id='rules'>
+          El primero en llegar a 10 puntos gana
+        </p>
+       <div className="score">
+          <p className='name_player'>You</p>
+          <p className='name_bot'>Bot</p>
+          <p className='points'>
+            <span className='points_player'>0</span>
+            <span>-</span>
+            <span className='points_bot'>0</span>
+          </p>
+       </div>
+       <div className="choose" id='choose'>
+        <h2>Choose an item</h2>
+        <div className="items">
+          <button className='item' id='rock'>🪨</button>
+          <button className='item' id='paper'>📝</button>
+          <button className='item' id='scissors'>✂️</button>
+        </div>
+       </div>
+       <div className='message' id='message'>
+        <p>
+          Your choise: <span className='choise' id='choise_player'>📝</span><br />
+          Bot choise: <span className='choise' id='choise_bot'>🪨</span>
+        </p>
+        <p id='win'> You win 🏆</p>
+       </div>
+       <button className='reboot'>Try again</button>
       </IonContent>
     </IonPage>
   );
